3067baf8f87a8681e2c38215a0a6c57e7c13bf15
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-06-20 20:55:09 +0700

    update

--- app/head.js
+++ app/head.js
@@ -5,11 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
       <meta name="description" content="MoneyApps" />
       <meta name="author" content="Ibrahim El Gibran" />
-      <link
-        rel="shortcut icon"
-        href="./assets/ieg.png"
-        type="image/x-icon"
-      ></link>
+      <link rel="shortcut icon" href="./bg.webp" type="image/x-icon"></link>
     </>
   );
 }
